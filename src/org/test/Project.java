72ab9adfb6584973d9714e91ecdecf36ffe8b697
sami9500 <sami29091994@gmail.com>
2021-09-18 21:04:12 +0530

    Branch Created

--- src/org/test/Project.java
+++ src/org/test/Project.java
@@ -7,9 +7,13 @@ public class Project {
 		private void name() {
 		System.out.println("sami");
 		}
+		private void address() {
+			System.out.println("vugfsjhbchhn");
+		}
 		public static void main(String[] args) {
 			Project g = new Project();
 			g.age();
 			g.name();
+			g.address();
 		}
 }
