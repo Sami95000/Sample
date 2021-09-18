04c10296a3c6b3ca0c2c352ba41ac52b887ae297
sami9500 <sami29091994@gmail.com>
2021-09-18 20:49:45 +0530

    Sample Created

---
+++ src/org/test/Project.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Project {
+	private void age() {
+		System.out.println("27");
+		}
+		private void name() {
+		System.out.println("sami");
+		}
+		public static void main(String[] args) {
+			Project g = new Project();
+			g.age();
+			g.name();
+		}
+}
