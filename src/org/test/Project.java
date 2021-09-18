3112e36b57f1aa3e07c81c83ab1cd9f054b7fcd3
sami9500 <sami29091994@gmail.com>
2021-09-18 21:14:29 +0530

    Branch2 Created

--- src/org/test/Project.java
+++ src/org/test/Project.java
@@ -7,9 +7,13 @@ public class Project {
 		private void name() {
 		System.out.println("sami");
 		}
+		private void email() {
+			System.out.println("samsh@gmail.com");
+		}
 		public static void main(String[] args) {
 			Project g = new Project();
 			g.age();
 			g.name();
+			g.email();
 		}
 }
